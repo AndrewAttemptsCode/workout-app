46f44e6523caf5eacd613b4151065e0d864a0162
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-25 19:27:40 +0100

    feat: add managing state to add new workout item and updating workout title

--- src/pages/WorkoutPage.tsx
+++ src/pages/WorkoutPage.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import WorkoutList from "../components/WorkoutList";
+
+export type Workout = {
+  workoutTitle: string;
+  exercises: string[];
+}
+
 const WorkoutPage = () => {
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+
+  const addNewWorkout = () => {
+    setWorkouts((prev) => [...prev, { workoutTitle: "", exercises: [] }]);
+  };
+
+  const updateWorkoutTitle = (index: number, newTitle: string) => {
+    setWorkouts((prev) =>
+      prev.map((workout, i) =>
+        i === index ? { ...workout, workoutTitle: newTitle } : workout
+      )
+    );
+  };
+
   return (
     <div>
-      Workout page
+      <h1>Workouts</h1>
+      <button onClick={addNewWorkout}>Add new workout</button>
+      <WorkoutList
+        workouts={workouts}
+        updateWorkoutTitle={updateWorkoutTitle}
+      />
     </div>
   );
 };
