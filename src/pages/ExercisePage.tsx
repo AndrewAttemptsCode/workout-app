5beb14fbf64236fbb7a3778c7cc32fdaa7a7a06d
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-27 15:43:57 +0100

    feat: add button to add a new exercise to exercises state

--- src/pages/ExercisePage.tsx
+++ src/pages/ExercisePage.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
+import ExerciseList from "../components/ExerciseList";
 
-type Exercise = {
+export type Exercise = {
   id: string;
   title: string;
   sets: { reps: number; weight: number }[];
-}
+};
 
 const ExercisePage = () => {
   const [exercises, setExercises] = useState<Exercise[]>([]);
 
+  const addNewExercise = () => {
+    setExercises((prev) => [
+      ...prev,
+      { id: crypto.randomUUID(), title: "", sets: [{ reps: 0, weight: 0 }] },
+    ]);
+  };
+
   return (
     <div>
       <h1>Exercises</h1>
+      <button onClick={addNewExercise}>Add new exercise</button>
+      <ExerciseList exercises={exercises} />
     </div>
   );
 };
