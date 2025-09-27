675f033961d982859016d9fbe534a4632ee846f8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-27 16:33:38 +0100

    feat: add exercise title input with initial styles and type props

--- src/components/ExerciseItem.tsx
+++ src/components/ExerciseItem.tsx
@@ -1,14 +1,34 @@
+import styled from "styled-components";
 import type { Exercise } from "../pages/ExercisePage";
+
+const Container = styled.div`
+  border: 2px solid black;
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
+`;
+
+const ExerciseTitle = styled.input`
+  padding: 0.5rem;
+  width: 100%;
+`;
 
 type ExerciseItemProps = {
   exercise: Exercise;
+  updateExerciseTitle: (id: string, newTitle: string ) => void;
 }
 
-const ExerciseItem = ({ exercise }: ExerciseItemProps) => {
+const ExerciseItem = ({ exercise, updateExerciseTitle }: ExerciseItemProps) => {
   return (
-    <div>
-      {exercise.id}
-    </div>
+    <Container>
+      <ExerciseTitle 
+        type="text"
+        name="exerciseTitle"
+        id="exerciseTitle"
+        placeholder="Exercise Name..."
+        value={exercise.title}
+        onChange={(e) => updateExerciseTitle(exercise.id, e.target.value)}
+      />
+    </Container>
   );
 };
 
