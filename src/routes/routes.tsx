022e3247fc1acef479aba8891db00996f4ab06cd
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-27 14:08:58 +0100

    feat: add exercises page route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import WorkoutPage from "../pages/WorkoutPage";
+import ExercisePage from "../pages/ExercisePage";
 
 const routes = createBrowserRouter([
   {
     path: "/workouts",
     element: <WorkoutPage />,
-  }
-])
+  },
+  {
+    path: "/exercises",
+    element: <ExercisePage />,
+  },
+]);
 
 export default routes;
