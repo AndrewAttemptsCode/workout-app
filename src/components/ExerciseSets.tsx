0ad3431f5cf74236fc4909a38ffe54057ba09151
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-28 15:04:12 +0100

    feat: add set rest period

--- src/components/ExerciseSets.tsx
+++ src/components/ExerciseSets.tsx
@@ -2,33 +2,49 @@ import type { Exercise } from "../pages/ExercisePage";
 
 type ExerciseSetsProps = {
   exercise: Exercise;
-  updateSetField: (exerciseId: string, setId: string, field: "reps" | "weight", value: number) => void;
+  updateSetField: (exerciseId: string, setId: string, field: "reps" | "weight" | "rest", value: number) => void;
 }
 
 const ExerciseSets = ({ exercise, updateSetField }: ExerciseSetsProps) => {
   return (
     <div>
       {exercise.sets.map((set) => (
-        <input
-          key={set.id}
-          type="number" 
-          name="reps"
-          min={0}
-          value={set.reps}
-          onChange={(e) => updateSetField(exercise.id, set.id, "reps", e.target.valueAsNumber)} 
-        />
-      ))} reps
-
-      {exercise.sets.map((set) => (
-        <input
-          key={set.id}
-          type="number" 
-          name="weight"
-          min={0}
-          value={set.weight}
-          onChange={(e) => updateSetField(exercise.id, set.id, "weight", e.target.valueAsNumber)} 
-        />
-      ))} kg
+        <div key={set.id}>
+          <input
+            type="number" 
+            name="reps"
+            id={`reps-${set.id}`}
+            min={0}
+            value={set.reps}
+            placeholder="Reps"
+            onChange={(e) => updateSetField(exercise.id, set.id, "reps", e.target.valueAsNumber)} 
+          />
+          <label htmlFor={`reps-${set.id}`}>reps</label>
+          
+          <input
+            type="number" 
+            name="weight"
+            id={`weight-${set.id}`}
+            min={0}
+            value={set.weight}
+            placeholder="Weight"
+            onChange={(e) => updateSetField(exercise.id, set.id, "weight", e.target.valueAsNumber)} 
+          />
+          <label htmlFor={`weight-${set.id}`}>kg</label>
+  
+          <input
+            type="number"
+            name="rest"
+            id={`rest-${set.id}`}
+            min={0}
+            value={set.rest}
+            placeholder="Rest"
+            onChange={(e) => updateSetField(exercise.id, set.id, "rest", e.target.valueAsNumber)}
+          />
+          <label htmlFor={`rest-${set.id}`}>secs</label>
+          
+        </div>
+      ))}
     </div>
   );
 };
