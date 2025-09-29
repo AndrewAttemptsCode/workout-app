b24bdc6b17bb2c375ce398ef781925114ac94f24
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-29 17:31:57 +0100

    feat: refactor sets reps to be displayed in a table and allow for edit and lock mode

--- src/components/ExerciseSets.tsx
+++ src/components/ExerciseSets.tsx
@@ -1,54 +1,105 @@
+import styled from "styled-components";
 import type { Exercise } from "../pages/ExercisePage";
 
 type ExerciseSetsProps = {
   exercise: Exercise;
   updateSetField: (exerciseId: string, setId: string, field: "reps" | "weight" | "rest", value: number) => void;
   removeSet: (exerciseId: string, setId: string) => void;
+  editMode: boolean;
 }
 
-const ExerciseSets = ({ exercise, updateSetField, removeSet }: ExerciseSetsProps) => {
+const Table = styled.table`
+  width: 100%;
+  table-layout: fixed;
+  border-collapse: separate;
+  border-spacing: 0.5rem;
+
+  td input {
+    width: 100%;
+  }
+
+  td span {
+    display: block;
+    width: 100%;
+  }
+
+  td {
+    text-align: center;
+  }
+
+  td button {
+    width: 50%;
+  }
+`;
+
+const ExerciseSets = ({ exercise, updateSetField, removeSet, editMode }: ExerciseSetsProps) => {
   return (
-    <div>
-      {exercise.sets.map((set) => (
-        <div key={set.id}>
-          <input
-            type="number" 
-            name="reps"
-            id={`reps-${set.id}`}
-            min={0}
-            value={set.reps}
-            placeholder="Reps"
-            onChange={(e) => updateSetField(exercise.id, set.id, "reps", e.target.valueAsNumber)} 
-          />
-          <label htmlFor={`reps-${set.id}`}>reps</label>
-          
-          <input
-            type="number" 
-            name="weight"
-            id={`weight-${set.id}`}
-            min={0}
-            value={set.weight}
-            placeholder="Weight"
-            onChange={(e) => updateSetField(exercise.id, set.id, "weight", e.target.valueAsNumber)} 
-          />
-          <label htmlFor={`weight-${set.id}`}>kg</label>
-  
-          <input
-            type="number"
-            name="rest"
-            id={`rest-${set.id}`}
-            min={0}
-            value={set.rest}
-            placeholder="Rest"
-            onChange={(e) => updateSetField(exercise.id, set.id, "rest", e.target.valueAsNumber)}
-          />
-          <label htmlFor={`rest-${set.id}`}>secs</label>
+    <Table>
+      <thead>
+        <tr>
+          <th>Reps</th>
+          <th>Weight (kg)</th>
+          <th>Rest (secs)</th>
+          {editMode && <th>Remove</th>}
+        </tr>
+      </thead>
+      <tbody>
+        {exercise.sets.map((set) => (
+          <tr key={set.id}>
+            <td>
+              {editMode ? (
+                <input
+                type="number" 
+                name="reps"
+                id={`reps-${set.id}`}
+                min={0}
+                value={set.reps}
+                onChange={(e) => updateSetField(exercise.id, set.id, "reps", e.target.valueAsNumber)} 
+              />
+              ) : (
+                <span>{set.reps}</span>
+              )}
+            </td>
+              
+            <td>
+              {editMode ? (
+                <input
+                  type="number" 
+                  name="weight"
+                  id={`weight-${set.id}`}
+                  min={0}
+                  value={set.weight}
+                  onChange={(e) => updateSetField(exercise.id, set.id, "weight", e.target.valueAsNumber)} 
+                />
+              ) : (
+                <span>{set.weight}</span>
+              )}
+            </td>
+      
+            <td>
+              {editMode ? (
+                <input
+                  type="number"
+                  name="rest"
+                  id={`rest-${set.id}`}
+                  min={0}
+                  value={set.rest}
+                  onChange={(e) => updateSetField(exercise.id, set.id, "rest", e.target.valueAsNumber)}
+                />
+              ) : (
+                <span>{set.rest}</span>
+              )}
+            </td>
 
-        <button disabled={exercise.sets.length <= 1} onClick={() => removeSet(exercise.id, set.id)}>Remove set</button>
-
-        </div>
-      ))}
-    </div>
+            {editMode && 
+              <td>
+                <button disabled={exercise.sets.length <= 1} onClick={() => removeSet(exercise.id, set.id)}>&times;</button>
+              </td>
+            }
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 };
 
