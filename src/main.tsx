2b6309109e15ff7940367fa1eefe32c74f286795
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-30 13:47:37 +0100

    feat: wrap routes with workout provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import routes from './routes/routes'
 import { RouterProvider } from 'react-router-dom'
+import { WorkoutProvider } from './contexts/WorkoutContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <WorkoutProvider>
+      <RouterProvider router={routes} />
+    </WorkoutProvider>
   </StrictMode>,
 )
