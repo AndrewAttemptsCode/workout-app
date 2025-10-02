2a49852fb6aeec2cce47c8d262e931628f27a0f7
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-02 17:20:38 +0100

    feat: update workout item to display fields based on editMode lock state

--- src/components/WorkoutItem.tsx
+++ src/components/WorkoutItem.tsx
@@ -1,5 +1,7 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { useWorkout, type Workout } from "../contexts/WorkoutContext";
+import { useState } from "react";
+import WorkoutExercisesDisplay from "./WorkoutExercisesDisplay";
 
 const Container = styled.div`
   border: 2px solid black;
@@ -7,9 +9,30 @@ const Container = styled.div`
   margin-bottom: 0.5rem;
 `;
 
-const WorkoutTitle = styled.input`
+const SetsContainer = styled.div`
+  height: 220px;
+  overflow-y: auto;
+`
+
+const sharedTitleStyles = css`
   padding: 0.5rem;
   width: 100%;
+`;
+
+const WorkoutTitle = styled.input`
+  ${sharedTitleStyles}
+`;
+
+const WorkoutDisplayTitle = styled.p`
+  ${sharedTitleStyles}
+  margin: none;
+  border: 2px solid transparent;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 0.5rem;
 `;
 
 type WorkoutItemProps = {
@@ -17,19 +40,47 @@ type WorkoutItemProps = {
 }
 
 const WorkoutItem = ({ workout }: WorkoutItemProps) => {
-  const { updateWorkoutTitle, removeWorkoutItem } = useWorkout();
+  const [editMode, setEditMode] = useState(workout.editStatus);
+  const { updateWorkoutTitle, removeWorkoutItem, updateWorkoutEditMode } = useWorkout();
 
   return (
     <Container>
-      <WorkoutTitle
-        type="text"
-        name="workoutTitle"
-        id="workoutTitle"
-        placeholder="Workout Name..."
-        value={workout.workoutTitle}
-        onChange={(e) => updateWorkoutTitle(workout.id, e.target.value)}
-      />
-      <button onClick={() => removeWorkoutItem(workout.id)}>Remove</button>
+
+      <ButtonContainer>
+        {editMode && (
+          <>
+            <button onClick={() => removeWorkoutItem(workout.id)}>Remove</button>
+          </>
+        )}
+
+        <button onClick={() => {
+          setEditMode((prev) => !prev);
+          updateWorkoutEditMode(workout.id, !editMode);
+        }}>
+          {editMode ? "Lock" : "Unlock"}
+        </button>
+      </ButtonContainer>
+
+      {editMode ? (
+        <WorkoutTitle
+          type="text"
+          name="workoutTitle"
+          id="workoutTitle"
+          placeholder="Workout Name..."
+          value={workout.workoutTitle}
+          onChange={(e) => updateWorkoutTitle(workout.id, e.target.value)}
+        />
+      ) : (
+        <WorkoutDisplayTitle>{workout.workoutTitle}</WorkoutDisplayTitle>
+      )}
+
+      <SetsContainer>
+        <WorkoutExercisesDisplay
+          exercises={workout.exercises}
+          editMode={editMode}
+        />
+      </SetsContainer>
+      
     </Container>
   );
 };
