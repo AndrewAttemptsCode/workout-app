8e80e44a89f27440b98e615d16a3d1fb86bc94f6
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-05 18:44:04 +0100

    style: add layout control  and initial background color

--- src/pages/WorkoutPage.tsx
+++ src/pages/WorkoutPage.tsx
@@ -1,15 +1,29 @@
+import styled from "styled-components";
 import WorkoutList from "../components/WorkoutList";
 import { useWorkout } from "../contexts/WorkoutContext";
+
+const StylesContainer = styled.div`
+  min-height: 100vh;
+  padding: 1rem 0;
+  background: linear-gradient(to bottom, #3f3f3f, #1a1919);
+`;
+
+const ResponsiveContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
 
 const WorkoutPage = () => {
   const { addNewWorkout } = useWorkout();
 
   return (
-    <div>
-      <h1>Workouts</h1>
-      <button onClick={addNewWorkout}>Add new workout</button>
-      <WorkoutList />
-    </div>
+    <StylesContainer>
+      <ResponsiveContainer>
+        <h1>Workouts</h1>
+        <button onClick={addNewWorkout}>Add new workout</button>
+        <WorkoutList />
+      </ResponsiveContainer>
+    </StylesContainer>
   );
 };
 
