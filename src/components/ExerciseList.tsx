740f19910f28e1a2f07216556aa8f3840c1b335e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-06 12:59:07 +0100

    style: add initial styling and layout of exercise items

--- src/components/ExerciseList.tsx
+++ src/components/ExerciseList.tsx
@@ -1,15 +1,22 @@
+import styled from "styled-components";
 import { useWorkout } from "../contexts/WorkoutContext";
 import ExerciseItem from "./ExerciseItem";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(290px, 1fr));
+  gap: 1rem;
+`;
 
 const ExerciseList = () => {
   const { exercises } = useWorkout(); 
 
   return (
-    <div>
+    <Container>
       {exercises.map((exercise) => (
         <ExerciseItem key={exercise.id} exercise={exercise} />
       ))}
-    </div>
+    </Container>
   );
 };
 
