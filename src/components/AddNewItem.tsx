494a5b325380ee9641597c39370236efea30577f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-08 17:25:28 +0100

    feat: add contents and styling

--- src/components/AddNewItem.tsx
+++ src/components/AddNewItem.tsx
@@ -1,8 +1,41 @@
+import { Dumbbell } from "lucide-react";
 import styled from "styled-components";
 
 const Button = styled.button`
   border: 2px dashed rgb(var(--gold-accent));
   background: transparent;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100%;
+  gap: 2rem;
+  font-size: 1.5rem;
+  color: rgb(var(--primary-color));
+  cursor: pointer;
+  -webkit-tap-highlight-color: transparent;
+  outline: none;
+  transition: border 0.3s ease;
+
+  svg {
+    transition: transform 0.3s ease;
+  }
+
+  &:focus-visible {
+    border: 2px dashed rgb(var(--green-accent));
+
+    svg {
+      transform: rotate(-180deg) scale(1.4);
+    }
+  }
+
+  &:hover {
+    border: 2px dashed rgb(var(--green-accent));
+
+    svg {
+      transform: rotate(-180deg) scale(1.4);
+    }
+  }
 `;
 
 type AddNewItemProps = {
@@ -12,7 +45,10 @@ type AddNewItemProps = {
 
 const AddNewItem = ({ onClick, title }: AddNewItemProps) => {
   return (
-    <Button onClick={onClick}>Add new {title}</Button>
+    <Button onClick={onClick} aria-label={`Add new ${title}`}>
+      <Dumbbell size={60} color="rgb(var(--primary-color))" />
+      Add new {title}
+    </Button>
   );
 };
 
