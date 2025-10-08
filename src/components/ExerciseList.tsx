7c583f8678d323548c037f82d7d9a19d2655bd6a
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-08 15:23:43 +0100

    feat: add AddNewItem to exercise list

--- src/components/ExerciseList.tsx
+++ src/components/ExerciseList.tsx
@@ -1,21 +1,24 @@
 import styled from "styled-components";
 import { useWorkout } from "../contexts/WorkoutContext";
 import ExerciseItem from "./ExerciseItem";
+import AddNewItem from "./AddNewItem";
 
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(290px, 1fr));
+  grid-auto-rows: 1fr;
   gap: 1rem;
 `;
 
 const ExerciseList = () => {
-  const { exercises } = useWorkout(); 
+  const { exercises, addNewExercise } = useWorkout(); 
 
   return (
     <Container>
       {exercises.map((exercise) => (
         <ExerciseItem key={exercise.id} exercise={exercise} />
       ))}
+      <AddNewItem onClick={addNewExercise} title="exercise" />
     </Container>
   );
 };
