62ce0d5cce6138fdb036ca8ee2cd40102e7ab73e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-13 15:22:23 +0100

    feat: add responsive layout and move workout timer card into its own component

--- src/pages/WorkoutTimerPage.tsx
+++ src/pages/WorkoutTimerPage.tsx
@@ -1,22 +1,26 @@
-import { useWorkout } from "../contexts/WorkoutContext";
+import styled from "styled-components";
+import WorkoutTimerCard from "../components/WorkoutTimerCard";
+
+const StylesContainer = styled.div`
+  min-height: 100vh;
+  min-height: 100dvh;
+  padding: 1rem 0;
+  background: linear-gradient(to bottom, #3f3f3f, #1a1919);
+`;
+
+const ResponsiveContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
 
 const WorkoutTimerPage = () => {
-  const { workoutTimer } = useWorkout();
 
   return (
-    <div>
-      <p>{workoutTimer?.workoutTitle}</p>
-      {workoutTimer?.exercises.map((exercise, index) => (
-        <div key={index}>
-          <p>{exercise?.exerciseTitle}</p>
-          {exercise?.sets.map((set, index) => (
-            <div key={index}>
-              <p>{set.reps} {set.weight} {set.rest}</p>
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
+    <StylesContainer>
+      <ResponsiveContainer>
+        <WorkoutTimerCard />
+      </ResponsiveContainer>
+    </StylesContainer>
   );
 };
 
