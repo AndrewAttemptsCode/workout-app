5f2ad91b7b16edb951b1493e58720704c787b861
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-13 17:18:07 +0100

    feat: add accordion style workout and exercise view

--- src/components/WorkoutTimerCard.tsx
+++ src/components/WorkoutTimerCard.tsx
@@ -1,21 +1,43 @@
+import { useState } from "react";
 import { useWorkout } from "../contexts/WorkoutContext";
 
 const WorkoutTimerCard = () => {
   const { workoutTimer } = useWorkout();
+  const [showExercises, setShowExercises] = useState(false);
+  const [showSets, setShowSets] = useState(-1);
 
   return (
     <div>
-      <p>{workoutTimer?.workoutTitle}</p>
-      {workoutTimer?.exercises.map((exercise, index) => (
-        <div key={index}>
-          <p>{exercise?.title}</p>
-          {exercise?.sets.map((set, index) => (
-            <div key={index}>
-              <p>{set.reps} {set.weight} {set.rest}</p>
-            </div>
-          ))}
-        </div>
-      ))}
+      <button
+        onClick={() => {
+          setShowExercises(!showExercises);
+          setShowSets(-1);
+        }}
+      >
+        {workoutTimer?.workoutTitle}
+      </button>
+
+      {showExercises &&
+        workoutTimer?.exercises.map((exercise, index) => (
+          <div key={index}>
+            <button
+              onClick={() =>
+                setShowSets((prev) => (prev === index ? -1 : index))
+              }
+            >
+              {exercise?.title}
+            </button>
+            
+            {showSets === index &&
+              exercise?.sets.map((set, index) => (
+                <div key={index}>
+                  <p>
+                    {set.reps} {set.weight} {set.rest}
+                  </p>
+                </div>
+              ))}
+          </div>
+        ))}
     </div>
   );
 };
