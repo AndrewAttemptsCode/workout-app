0eb123d94cdc98d6772cce0365b27e1785a56025
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-19 18:37:07 +0100

    feat: update control button to start the rest timer from useWorkoutTimer context and disabled based off if the timer is active

--- src/components/WorkoutTimerControls.tsx
+++ src/components/WorkoutTimerControls.tsx
@@ -1,10 +1,19 @@
 import { useWorkout } from "../contexts/WorkoutContext";
+import { useWorkoutTimer } from "../contexts/WorkoutTimerContext";
 
 const WorkoutTimerControls = () => {
-  const { handleWorkoutTimerComplete, workoutTimer } = useWorkout();
+  const { workoutTimer, currentProgress } = useWorkout();
+  const { startTimer, secondsLeft, timerActive } = useWorkoutTimer();
+
+  const setRestTime = currentProgress?.set?.rest ?? 0;
 
   return (
-    <button onClick={handleWorkoutTimerComplete}>{workoutTimer?.complete ? "Finish" : "Complete"}</button>
+    <button
+      onClick={() => startTimer(setRestTime)}
+      disabled={timerActive}
+    >
+      { secondsLeft } {workoutTimer?.complete ? "Finish" : "Complete"}
+    </button>
   );
 };
 
