be82df42c48e5936922a925e2a08d03459370d4f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-20 19:22:50 +0100

    style: add layout control and colors for when button available and disabled

--- src/components/WorkoutTimerControls.tsx
+++ src/components/WorkoutTimerControls.tsx
@@ -1,19 +1,47 @@
+import styled from "styled-components";
 import { useWorkout } from "../contexts/WorkoutContext";
 import { useWorkoutTimer } from "../contexts/WorkoutTimerContext";
 
+const Container = styled.div`
+  width: min(100%, 768px);
+  margin: 0 auto;
+
+  button {
+    padding: 0.5rem;
+    min-height: 44px;
+    width: 100%;
+    cursor: pointer;
+    outline: none;
+    -webkit-tap-highlight-color: transparent;
+    text-transform: uppercase;
+    font-weight: bold;
+    color: rgb(var(--primary-color));
+    border: 2px solid rgb(var(--green-accent));
+    background: rgba(var(--green-accent), 0.6);
+
+    &:disabled {
+      color: rgba(var(--primary-color), 0.5);
+      border: 2px solid rgba(var(--primary-color), 0.4);
+      background: rgba(var(--primary-color), 0.3);
+    }
+  }
+`;
+
 const WorkoutTimerControls = () => {
   const { workoutTimer, currentProgress } = useWorkout();
-  const { startTimer, secondsLeft, timerActive } = useWorkoutTimer();
+  const { startTimer, timerActive } = useWorkoutTimer();
 
   const setRestTime = currentProgress?.set?.rest ?? 0;
 
   return (
-    <button
-      onClick={() => startTimer(setRestTime)}
-      disabled={timerActive}
-    >
-      { secondsLeft } {workoutTimer?.complete ? "Finish" : "Complete"}
-    </button>
+    <Container>
+      <button
+        onClick={() => startTimer(setRestTime)}
+        disabled={timerActive}
+      >
+        {workoutTimer?.complete ? "Finish" : "Complete Set"}
+      </button>
+    </Container>
   );
 };
 
