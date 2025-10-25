a15bc9d778f4e21dac9eec7d35437d345cb9a24c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-10-25 17:14:10 +0100

    feat: update routes to have an app layout with children

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,19 +2,27 @@ import { createBrowserRouter } from "react-router-dom";
 import WorkoutPage from "../pages/WorkoutPage";
 import ExercisePage from "../pages/ExercisePage";
 import WorkoutTimerPage from "../pages/WorkoutTimerPage";
+import AppLayout from "../components/AppLayout";
 
 const routes = createBrowserRouter([
   {
-    path: "/workouts",
-    element: <WorkoutPage />,
-  },
-  {
-    path: "/exercises",
-    element: <ExercisePage />,
-  },
-  {
-    path: "/timer",
-    element: <WorkoutTimerPage />,
+    path: "/",
+    element: <AppLayout />,
+    // errorElement: <ErrorBoundary />,
+    children: [
+    {
+      path: "/workouts",
+      element: <WorkoutPage />,
+    },
+    {
+      path: "/exercises",
+      element: <ExercisePage />,
+    },
+    {
+      path: "/timer",
+      element: <WorkoutTimerPage />,
+    },
+    ],
   },
 ]);
 
