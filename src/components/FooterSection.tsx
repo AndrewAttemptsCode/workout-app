57a68b269e0e49dbb0b81a445323e3839e8aaf22
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-01 15:42:47 +0000

    feat: add footer section containing the logo and socials

---
+++ src/components/FooterSection.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import HeaderLogo from "./HeaderLogo";
+import Socials from "./Socials";
+
+const Container = styled.footer`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding-top: 2rem;
+
+  hr {
+    width: 90%;
+    margin: 0 auto;
+  }
+`;
+
+const LayoutPad = styled.div`
+  padding: 2rem 0;
+`;
+
+const FooterSection = () => {
+  return (
+    <Container>
+      <hr />
+      <LayoutPad>
+        <HeaderLogo />
+        <Socials />
+      </LayoutPad>
+    </Container>
+  );
+};
+
+export default FooterSection;
