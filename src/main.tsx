b948d2ebf8b6c08cf693131c73ed972d13660157
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-02 20:31:06 +0000

    feat: wrap route with dashboard context provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import routes from './routes/routes'
 import { RouterProvider } from 'react-router-dom'
 import { WorkoutProvider } from './contexts/WorkoutContext'
+import { DashboardProvider } from './contexts/DashboardContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <WorkoutProvider>
-      <RouterProvider router={routes} />
-    </WorkoutProvider>
+    <DashboardProvider>
+      <WorkoutProvider>
+        <RouterProvider router={routes} />
+      </WorkoutProvider>
+    </DashboardProvider>
   </StrictMode>,
 )
