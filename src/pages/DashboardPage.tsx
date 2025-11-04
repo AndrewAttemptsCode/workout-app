5b06b80757516103ccbde760226ff83dc46809d0
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-04 19:36:54 +0000

    feat: add inital dashboard page with dashboard stats component

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,11 @@
+import DashboardStats from "../components/DashboardStats";
+
+const DashboardPage = () => {
+  return (
+    <>
+      <DashboardStats />
+    </>
+  );
+};
+
+export default DashboardPage;
