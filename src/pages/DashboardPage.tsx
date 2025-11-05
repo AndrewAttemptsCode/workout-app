81015bca06ed54fb3900277d3944952cd4723f6c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-05 19:48:17 +0000

    feat: add dashboard weekly workout tracker to the dashboard page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,8 +1,10 @@
 import DashboardStats from "../components/DashboardStats";
+import DashboardWeeklyTracker from "../components/DashboardWeeklyTracker";
 
 const DashboardPage = () => {
   return (
     <>
+      <DashboardWeeklyTracker />
       <DashboardStats />
     </>
   );
