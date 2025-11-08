3b3d3e65e25110be6269e819a568cb50cb15d619
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-08 21:19:47 +0000

    feat: add monthly workout charts component to the page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,13 @@
 import DashboardStats from "../components/DashboardStats";
 import DashboardWeeklyTracker from "../components/DashboardWeeklyTracker";
+import MonthlyWorkoutChart from "../components/MonthlyWorkoutChart";
 
 const DashboardPage = () => {
   return (
     <>
       <DashboardWeeklyTracker />
       <DashboardStats />
+      <MonthlyWorkoutChart />
     </>
   );
 };
