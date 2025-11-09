113a0ee447873e8eb2155953f0fee78a1c246fde
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-09 19:11:34 +0000

    style: update to include a subtitle and general layout control of stat items with its own container

--- src/components/DashboardStats.tsx
+++ src/components/DashboardStats.tsx
@@ -9,6 +9,26 @@ const StylesContainer = styled.div`
 const ResponsiveContainer = styled.div`
   width: min(90%, 1024px);
   margin: 0 auto;
+
+    h2 {
+      line-height: 1;
+      margin-bottom: 1rem;
+      text-transform: uppercase;
+      font-size: 1rem;
+      display: flex;
+      align-items: center;
+
+      &::after  {
+        content: "";
+        flex: 1;
+        height: 2px;
+        background: rgb(var(--primary-color));
+        margin-left: 1rem;
+      }
+    }
+`;
+
+const StatsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   grid-auto-rows: 1fr;
@@ -17,16 +37,15 @@ const ResponsiveContainer = styled.div`
 
 const StatItem = styled.div`
   background: rgba(var(--primary-color), 0.2);
-  padding: 1rem;
+  padding: 1rem 0.5rem;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  text-align: center;
   gap: 1rem;
 `;
 
-const StatName = styled.h2`
+const StatName = styled.h3`
   font-size: 1rem;
 `;
 
@@ -41,41 +60,44 @@ const DashboardStats = () => {
   return (
     <StylesContainer>
       <ResponsiveContainer>
-        {stats.map(stat => {
-          let displayValue: number | string;
+        <h2>Quick Stats</h2>
+        <StatsContainer>
+          {stats.map(stat => {
+            let displayValue: number | string;
 
-          if (typeof stat.value === "number") {
-            if (stat.name === "Heaviest weight lifted") {
-              displayValue = Number(stat.value) + " kg";
-            } else {
-              displayValue = Number(stat.value);
-            }
-          } else if (stat.name === "Last worked out") {
-            if (stat.value !== "None yet") {
-              const currentDate = new Date();
-              displayValue = formatDistance(new Date(stat.value), currentDate, {
-                addSuffix: true,
-                includeSeconds: true,
-              });
+            if (typeof stat.value === "number") {
+              if (stat.name === "Heaviest weight lifted") {
+                displayValue = Number(stat.value) + " kg";
+              } else {
+                displayValue = Number(stat.value);
+              }
+            } else if (stat.name === "Last worked out") {
+              if (stat.value !== "None yet") {
+                const currentDate = new Date();
+                displayValue = formatDistance(new Date(stat.value), currentDate, {
+                  addSuffix: true,
+                  includeSeconds: true,
+                });
+              } else {
+                displayValue = String(stat.value);
+              }
             } else {
               displayValue = String(stat.value);
             }
-          } else {
-            displayValue = String(stat.value);
-          }
-          
-          return (
-            <StatItem key={stat.name}>
-              <StatName>
-                {stat.name}
-              </StatName>
-              <StatValue>
-                {displayValue}
-              </StatValue>
-            </StatItem>  
-          )
-          })
-        }      
+            
+            return (
+              <StatItem key={stat.name}>
+                <StatName>
+                  {stat.name}
+                </StatName>
+                <StatValue>
+                  {displayValue}
+                </StatValue>
+              </StatItem>  
+            )
+            })
+          }      
+        </StatsContainer>
       </ResponsiveContainer>
     </StylesContainer>
   );
