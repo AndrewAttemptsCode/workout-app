f588940ccb826a9f46864e71b088c2db7d19fbc5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-10 19:37:13 +0000

    style: add button styling

--- src/components/DashResetButton.tsx
+++ src/components/DashResetButton.tsx
@@ -3,11 +3,33 @@ import styled from "styled-components";
 
 const Button = styled.button`
   aspect-ratio: 1 / 1;
+  background: rgba(var(--primary-color), 0.2);
+  color: inherit;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 4rem;
+  -webkit-tap-highlight-color: transparent;
+  transition: box-shadow 0.3s ease, background 0.3s ease;
+    
+  &:focus-visible {
+    box-shadow: 0 0 4px 2px rgb(var(--red-accent));
+    background: rgba(var(--red-accent), 0.2);
+  }
+
+  &:hover {
+    box-shadow: 0 0 4px 2px rgb(var(--red-accent));
+    background: rgba(var(--red-accent), 0.2);
+  }
 `;
 
-const DashResetButton = () => {
+type DashResetButtonProps = {
+  value: string;
+}
+
+const DashResetButton = ({ value }: DashResetButtonProps) => {
   return (
-    <Button>
+    <Button title="Reset Stats" aria-label={`Reset stats for ${value}`}>
       <RiResetLeftLine />
     </Button>
   );
