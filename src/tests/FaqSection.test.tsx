5a93757caaaa4de7299d4af6fd69089aff6d256b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-11 21:30:12 +0000

    feat: add test for accordion action

--- src/tests/FaqSection.test.tsx
+++ src/tests/FaqSection.test.tsx
@@ -9,7 +9,7 @@ describe("FAQ section component", () => {
     render(<FaqSection />);
     const question = screen.getByText("Do I need an account?");
     const answer = screen.getByText("No, you do not need an account to use this app.");
-    
+
     // Initial render - starts as hidden
     expect(answer).not.toBeVisible();
     
@@ -20,6 +20,26 @@ describe("FAQ section component", () => {
     // User clicks same button - ends as hidden
     await userEvent.click(question);
     expect(answer).not.toBeVisible();
+  });
+
+  it("closes the original answer when a new question is opened and displays the new answer", async () => {
+    render(<FaqSection />);
+    const question = screen.getByText("Do I need an account?");
+    const answer = screen.getByText("No, you do not need an account to use this app.");
+    const question2 = screen.getByText("Is my data stored online?");
+    const answer2 = screen.getByText("No, everything is stored locally in your device browser.");
+    
+    // Initial render - starts as hidden
+    expect(answer).not.toBeVisible();
+    
+    // User clicks button - answer visible
+    await userEvent.click(question);
+    expect(answer).toBeVisible();
+
+    // User clicks another button - original as hidden and new as visible
+    await userEvent.click(question2);
+    expect(answer).not.toBeVisible();
+    expect(answer2).toBeVisible();
   });
 
 });
