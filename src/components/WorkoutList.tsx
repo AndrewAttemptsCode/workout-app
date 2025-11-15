a2ab92f5165beec5e62d115ebaa5033142fc09fe
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-15 19:52:16 +0000

    refactor: update so add new item is added to the end of the grid and add a heading to the section with a screen reader only title

--- src/components/WorkoutList.tsx
+++ src/components/WorkoutList.tsx
@@ -2,16 +2,17 @@ import WorkoutItem from "./WorkoutItem";
 import { useWorkout } from "../contexts/WorkoutContext";
 import styled from "styled-components";
 import AddNewItem from "./AddNewItem";
+import SrOnly from "./SrOnly";
 
 const Container = styled.section`
+`;
+
+const WorkoutsWrapper = styled.ul`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(290px, 1fr));
   grid-auto-rows: 1fr;
   min-height: 300px;
   gap: 1rem;
-`;
-
-const WorkoutsWrapper = styled.ul`
   padding: 0;
   list-style: none;
 `;
@@ -20,13 +21,17 @@ const WorkoutList = () => {
   const { workouts, addNewWorkout } = useWorkout();
   
   return (
-    <Container aria-label="My workouts">
+    <Container aria-labelledby="workouts-heading">
+      <SrOnly><h2 id="workouts-heading">My Workouts</h2></SrOnly>
+      
       <WorkoutsWrapper>
         {workouts.map((workout) => (
           <WorkoutItem key={workout.id} workout={workout} />
         ))}
+        <li>
+          <AddNewItem onClick={addNewWorkout} title="workout" />
+        </li>
       </WorkoutsWrapper>
-      <AddNewItem onClick={addNewWorkout} title="workout" />
     </Container>
   );
 };
