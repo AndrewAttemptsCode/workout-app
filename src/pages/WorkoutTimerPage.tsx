ddd870e0c7eec56d5b7b9b96125069df5d15b55e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-18 17:47:51 +0000

    style: remove background as it is controlled by AppLayoutSecondary element

--- src/pages/WorkoutTimerPage.tsx
+++ src/pages/WorkoutTimerPage.tsx
@@ -8,7 +8,6 @@ const StylesContainer = styled.div`
   min-height: 100vh;
   min-height: 100dvh;
   padding: 1rem 0;
-  background: linear-gradient(to bottom, #3f3f3f, #1a1919);
 `;
 
 const ResponsiveContainer = styled.div`
