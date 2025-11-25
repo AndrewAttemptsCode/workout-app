40d29f402e67b44e3814947c10ecfdae807d73a8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-11-25 13:44:00 +0000

    test: load workout button on a workout item loads the workout into the workout timer page

--- src/tests/Workouts.test.tsx
+++ src/tests/Workouts.test.tsx
@@ -6,6 +6,8 @@ import WorkoutPage from "../pages/WorkoutPage";
 import { WorkoutProvider } from "../contexts/WorkoutContext";
 import { MemoryRouter } from "react-router-dom";
 import ExercisePage from "../pages/ExercisePage";
+import WorkoutTimerPage from "../pages/WorkoutTimerPage";
+import { DashboardProvider } from "../contexts/DashboardContext";
 
 const renderComponent = (children: React.ReactNode) => {
   render(
@@ -190,5 +192,57 @@ describe("Workouts page", () => {
     const removeExerciseButton = within(workoutList).getByRole("button", { name: /remove bench press from workout/i });
     await userEvent.click(removeExerciseButton);
     expect(exercise).not.toBeInTheDocument();
+  })
+
+  it("workout is loaded into workout timer", async () => {
+    renderComponent(
+      <>
+        <WorkoutPage />
+        <ExercisePage />
+        <DashboardProvider>
+          <WorkoutTimerPage />
+        </DashboardProvider>
+      </>
+    )
+
+    // Add workout item to the workout list
+    const addWorkoutButton = screen.getByRole("button", { name: /add new workout/i });
+    await userEvent.click(addWorkoutButton);
+
+    // Add exercise item to the exercise list
+    const addExerciseButton = screen.getByRole("button", { name: /add new exercise/i });
+    await userEvent.click(addExerciseButton);
+
+    // Give exercise an accessible name
+    const exerciseList = screen.getByRole("region", { name: /my exercises/i });
+    const exerciseTitle = within(exerciseList).getByRole("textbox", { name: /exercise name/i });
+    await userEvent.click(exerciseTitle);
+    await userEvent.clear(exerciseTitle);
+    await userEvent.type(exerciseTitle, "bench press");
+
+    // Lock exercise item "edit item mode", to access add to workout button
+    const editButton = within(exerciseList).getByRole("button", { name: /edit item/i });
+    await userEvent.click(editButton);
+
+    // Add exercise to workout item exercise list
+    const addToWorkoutButton = screen.getByRole("button", { name: /add to workout/i });
+    await userEvent.click(addToWorkoutButton);
+
+    const workoutItem = screen.getByRole("button", { name: /^add exercise to/i });
+    await userEvent.click(workoutItem);
+
+    // Lock edit item mode on the workout item
+    const workoutList = screen.getByRole("region", { name: /my workouts/i });
+    const workoutItemEditButton = within(workoutList).getByRole("button", { name: /edit item/i });
+    await userEvent.click(workoutItemEditButton);
+
+    // Load workout into the workout timer
+    const loadWorkoutButton = screen.getByRole("button", { name: /load workout/i });
+    await userEvent.click(loadWorkoutButton);
+
+    // Expect to see workout exercise within the workout timer display
+    const timerDisplay = screen.getByRole("region", { name: /workout timer display/i });
+    const exerciseInDisplay = within(timerDisplay).getByText(/bench press/i); 
+    expect(exerciseInDisplay).toBeInTheDocument();
   })
 });
