f73e54788e8c2bbfcb58c4964a28d12507ff1718
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-26 20:26:09 +0100

    feat: add method to assert socials have the correct href links

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,15 +1,17 @@
-import { Locator, Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 
 class HomePage {
   private readonly page: Page;
   private readonly workoutButton: Locator;
   private readonly faqItems: Locator;
+  private readonly footer: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.workoutButton = this.page.getByRole("button", { name: /get started/i });
     const faqSection = this.page.locator("section").filter({ has: this.page.getByRole("heading", { name: /faq/i })});
     this.faqItems = faqSection.getByRole("button");
+    this.footer = this.page.locator("footer");
   }
 
   async clickStartWorkout() {
@@ -19,6 +21,13 @@ class HomePage {
   async faqItem(faqItemIndex: number) {
     const items = await this.faqItems.all();
     return items[faqItemIndex];
+  }
+
+  async assertSocialLinks(socials: Record<string, string>) {
+    for (const [key, expectedHref] of Object.entries(socials)) {
+      const link = this.footer.getByRole("link", { name: key });
+      await expect(link).toHaveAttribute("href", expectedHref);
+    };
   }
 }
 
