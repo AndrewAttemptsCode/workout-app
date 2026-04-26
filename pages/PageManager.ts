5a9520c5f940382df43924450e732f93eac2aea5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-26 17:27:55 +0100

    feat: add homepage page object model and remove private from this.page

--- pages/PageManager.ts
+++ pages/PageManager.ts
@@ -1,17 +1,24 @@
 import { type Page } from "@playwright/test";
 import NavBar from "./Navbar";
+import HomePage from "./HomePage";
 
 class PageManager {
-  private readonly page: Page;
+  readonly page: Page;
   private readonly navbar: NavBar;
+  private readonly homepage: HomePage;
 
   constructor(page: Page) {
     this.page = page;
     this.navbar = new NavBar(this.page);
+    this.homepage = new HomePage(this.page);
   }
 
   nav() {
     return this.navbar;
+  }
+
+  home() {
+    return this.homepage;
   }
 }
 
