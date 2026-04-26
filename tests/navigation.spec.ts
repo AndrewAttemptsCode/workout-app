373b75c999ed6054d5068b0bb5747672ec95f8fc
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-26 17:27:10 +0100

    refactor: update ot use page manager fixture this.page

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -2,24 +2,24 @@ import { expect } from "@playwright/test";
 import { test } from "../test-options";
 
 test.describe("Navbar functionality", () => {
-  test("user can navigate to homepage", async ({ page, pm }) => {
+  test("user can navigate to homepage", async ({ pm }) => {
     await pm.nav().goToHome();
-    await expect(page.getByRole("heading", { name: /track. train. transform./i })).toBeVisible();
+    await expect(pm.page.getByRole("heading", { name: /track. train. transform./i })).toBeVisible();
   });
 
-  test("user can navigate to workouts page", async ({ page, pm }) => {
+  test("user can navigate to workouts page", async ({ pm }) => {
     await pm.nav().goToWorkouts();
-    await expect(page.getByRole("button", { name: /add new workout/i })).toBeVisible();
+    await expect(pm.page.getByRole("button", { name: /add new workout/i })).toBeVisible();
   });
 
-  test("user can navigate to exercises page", async ({ page, pm }) => {
+  test("user can navigate to exercises page", async ({ pm }) => {
     await pm.nav().goToExercises();
-    await expect(page.getByRole("button", { name: /add new exercise/i })).toBeVisible();
+    await expect(pm.page.getByRole("button", { name: /add new exercise/i })).toBeVisible();
   });
 
-  test("user can navigate to dashboard page", async ({ page, pm }) => {
+  test("user can navigate to dashboard page", async ({ pm }) => {
     await pm.nav().goToDashboard();
-    await expect(page.getByRole("heading", { name: /weekly tracker/i })).toBeVisible();
+    await expect(pm.page.getByRole("heading", { name: /weekly tracker/i })).toBeVisible();
   });
 
 });
