53a309989eeba54ae81ccbedd86bb2a5c0761372
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-26 18:06:19 +0100

    feat: add faqItem method to return a single faq button item

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -3,14 +3,22 @@ import { Locator, Page } from "@playwright/test";
 class HomePage {
   private readonly page: Page;
   private readonly workoutButton: Locator;
+  private readonly faqItems: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.workoutButton = this.page.getByRole("button", { name: /get started/i });
+    const faqSection = this.page.locator("section").filter({ has: this.page.getByRole("heading", { name: /faq/i })});
+    this.faqItems = faqSection.getByRole("button");
   }
 
   async clickStartWorkout() {
     await this.workoutButton.click();
+  }
+
+  async faqItem(faqItemIndex: number) {
+    const items = await this.faqItems.all();
+    return items[faqItemIndex];
   }
 }
 
