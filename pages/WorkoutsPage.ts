c1a429e831389be041ef3e367a3a99fd25b24ec3
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-27 22:36:13 +0100

    feat: add method to remove workout item by specifying item index and clicking its remove item button

--- pages/WorkoutsPage.ts
+++ pages/WorkoutsPage.ts
@@ -3,16 +3,23 @@ import { Locator, Page } from "@playwright/test";
 class WorkoutsPage {
   readonly page: Page;
   private readonly addWorkoutButton: Locator;
+  private readonly workoutItems: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.addWorkoutButton = this.page.getByRole("button", { name: /add new workout/i });
+    this.workoutItems = this.page.getByTestId("workout-item");
   }
 
   async addNewWorkout() {
     await this.addWorkoutButton.click();
   }
-  
+
+  async removeWorkoutAt(index: number) {
+    const workoutItem = this.workoutItems.nth(index);
+    await workoutItem.getByRole("button", { name: /remove item/i }).click();
+  }
+
 }
 
 export default WorkoutsPage;
