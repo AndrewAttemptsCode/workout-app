69c8b157f902e42bc61c9bc1add99da51b381104
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-27 21:00:41 +0100

    feat: add WorkoutsPage POM to page manager as method

--- pages/PageManager.ts
+++ pages/PageManager.ts
@@ -1,16 +1,19 @@
 import { type Page } from "@playwright/test";
 import NavBar from "./Navbar";
 import HomePage from "./HomePage";
+import WorkoutsPage from "./WorkoutsPage";
 
 class PageManager {
   readonly page: Page;
   private readonly navbar: NavBar;
   private readonly homepage: HomePage;
+  private readonly workoutspage: WorkoutsPage;
 
   constructor(page: Page) {
     this.page = page;
     this.navbar = new NavBar(this.page);
     this.homepage = new HomePage(this.page);
+    this.workoutspage = new WorkoutsPage(this.page);
   }
 
   nav() {
@@ -19,6 +22,10 @@ class PageManager {
 
   home() {
     return this.homepage;
+  }
+
+  workouts() {
+    return this.workoutspage;
   }
 }
 
