ccfffe4eef45068e0b6eb416ddc9350f4349ec25
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-30 19:14:48 +0100

    feat: add exercises page as method to page manager

--- pages/PageManager.ts
+++ pages/PageManager.ts
@@ -2,18 +2,21 @@ import { type Page } from "@playwright/test";
 import NavBar from "./Navbar";
 import HomePage from "./HomePage";
 import WorkoutsPage from "./WorkoutsPage";
+import ExercisesPage from "./ExercisesPage";
 
 class PageManager {
   readonly page: Page;
   private readonly navbar: NavBar;
   private readonly homepage: HomePage;
   private readonly workoutspage: WorkoutsPage;
+  private readonly exercisespage: ExercisesPage;
 
   constructor(page: Page) {
     this.page = page;
     this.navbar = new NavBar(this.page);
     this.homepage = new HomePage(this.page);
     this.workoutspage = new WorkoutsPage(this.page);
+    this.exercisespage = new ExercisesPage(this.page);
   }
 
   nav() {
@@ -26,6 +29,10 @@ class PageManager {
 
   workouts() {
     return this.workoutspage;
+  }
+
+  exercises() {
+    return this.exercisespage;
   }
 }
 
