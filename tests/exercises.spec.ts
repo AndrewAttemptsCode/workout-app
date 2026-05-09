3ccc6915635cb6c4449632a535860d90fb660484
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-05-09 14:43:43 +0100

    test: add exercise to workout item

--- tests/exercises.spec.ts
+++ tests/exercises.spec.ts
@@ -305,4 +305,21 @@ test.describe("Exercises functionality", () => {
     await expect(exerciseSets).toBeVisible();
   });
 
+  test("add exercise to workout", async ({ pm }) => {
+    await pm.nav().goToWorkouts();
+    await pm.workouts().addNewWorkout();
+    await pm.workouts().setWorkoutTitle(0, "upper body");
+
+    await pm.nav().goToExercises();
+    await pm.exercises().addNewExercise();
+    await pm.exercises().setExerciseTitle(0, "shoulder press");
+
+    await pm.exercises().addExerciseToWorkout(0, 0);
+
+    await pm.nav().goToWorkouts();
+    const workoutItem = pm.workouts().getWorkoutAt(0);
+
+    await expect(workoutItem).toContainText("shoulder press");
+  });
+
 });
