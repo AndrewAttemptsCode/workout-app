86f1ce1e8b8f9fc9b2a6ff63e7025992354b38bb
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-05-18 17:24:35 +0100

    feat: add method to assert if workout weekly tracker marks as complete

--- pages/DashboardPage.ts
+++ pages/DashboardPage.ts
@@ -1,10 +1,44 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 
 class DashboardPage {
   private readonly page: Page;
 
   constructor(page: Page) {
     this.page = page;
+  }
+
+  weeklyTracker() {
+    return this.page.getByRole("region", { name: /weekly tracker/i });
+  }
+
+  getWeeklyTrackerDays() {
+    return this.weeklyTracker().getByRole("listitem");
+  }
+
+  async assertWeeklyDayComplete() {
+    const days = this.getWeeklyTrackerDays();
+
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    const todaysDay = daysOfWeek[new Date().getDay()];
+
+    for (const day of await days.all()) {
+      const text = await day.textContent();
+
+      if (text?.includes(todaysDay)) {
+        await expect(day).toContainText(/is complete$/i);
+      } else {
+        await expect(day).toContainText(/is not complete$/i);
+      }
+    }
   }
 }
 
