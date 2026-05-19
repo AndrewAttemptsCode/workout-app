40835cfff029bd5a0971b97ae2917817b864c3c2
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-05-19 18:54:24 +0100

    test: dashboard quick stats render

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -1,3 +1,4 @@
+import { expect } from "@playwright/test";
 import { test } from "../test-options";
 
 test.describe("Dashboard functionality", () => {
@@ -11,5 +12,16 @@ test.describe("Dashboard functionality", () => {
     await pm.dashboard().resetStats("weekly tracker");
 
     await pm.dashboard().assertAllDaysIncomplete();
+  });
+
+  test("quick stats render", async ({ pm, completeWorkout: _ }) => {
+    await expect(pm.dashboard().quickStat("last worked out")).toBeVisible();
+    await expect(pm.dashboard().quickStat("exercises complete")).toBeVisible();
+    await expect(pm.dashboard().quickStat("heaviest weight lifted")).toBeVisible();
+    await expect(pm.dashboard().quickStat("last workout complete")).toBeVisible();
+    await expect(pm.dashboard().quickStat("reps complete")).toBeVisible();
+    await expect(pm.dashboard().quickStat("sets complete")).toBeVisible();
+    await expect(pm.dashboard().quickStat("total workout duration")).toBeVisible();
+    await expect(pm.dashboard().quickStat("workouts complete")).toBeVisible();
   });
 });
